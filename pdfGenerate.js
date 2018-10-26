2db4d566d7b06f100b2a038ebe9704ab76304fab
Sagar Pednekar <sagar.pednekar@accionlabs.com>
2018-10-26 18:33:26 +0530

    Added templates for pdf

--- pdfGenerate.js
+++ pdfGenerate.js
@@ -3,17 +3,17 @@ const sharp = require("sharp");
 const Prince = require("prince");
 const fs = require("fs");
 let content = `<style> hr {} @page { size: A4; prince-shrink-to-fit: auto; @bottom { content: "Page " counter(page) " of " counter(pages) } } img { page: image; display: block; }  } @page content{ size: A4; prince-shrink-to-fit: auto; @bottom { content: counter(page) }}  #seprate{page: content; page-break-after: always } </style>`;
-let contentWithNumbering = `<style> hr {} @page { size: A4; prince-shrink-to-fit: auto; @bottom { content:  counter(page) } } img { page: image; display: block; }  } @page content{ size: A4; prince-shrink-to-fit: auto; @bottom { content: counter(page) }}  #seprate{page: content; page-break-after: always } </style>`; // with 
+let contentWithNumbering = `<style> hr {} @page { size: A4; prince-shrink-to-fit: auto; @bottom { content:  counter(page) } } img { page: image; display: block; }  } @page content{ size: A4; prince-shrink-to-fit: auto; @bottom { content: counter(page) }}  #seprate{page: content; page-break-after: always } </style>`; // with
 
 const config = { type: "cover" };
 const pages = [
   ` <img src="./output/front-page.png"  alt="">`,
   `<img src="./output/spine.png"  alt="">`,
   `<img src="./output/back-page.png"  alt="">`,
-  `<h1>Hello this new page</h1>`,
+  `<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>`,
   `<h1>This is page 3</h1>`
 ];
-const finalHtml = generateHtml(contentWithNumber, pages);
+const finalHtml = generateHtml(contentWithNumbering, pages);
 
 function generateHtml(content, newPage) {
   newPage.map(element => {
@@ -43,7 +43,7 @@ function princePdf() {
   });
 }
 
-function webShot() {
+function webShot(input) {
   const options = {
     windowSize: {
       width: 1024,
@@ -53,12 +53,18 @@ function webShot() {
       width: 3325,
       height: 2475
     },
+    shotOffset: {
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0
+    },
     siteType: "file",
     streamType: "png",
     orientation: "portrait"
   };
   return new Promise((resolve, reject) => {
-    webshot("test.html", "webshot.png", options, function(err) {
+    webshot(input, "webshot.png", options, function(err) {
       if (err) reject(err);
       console.log("Generating screenshot.....");
       resolve();
@@ -66,10 +72,10 @@ function webShot() {
   });
 }
 
-function sharpBackBPage() {
+function sharpBackBPage(input) {
   return (
-    sharp("./webshot.png")
-      .extract({ left: 0, top: 0, width: 1664, height: 2475 }) //({ left: left, top: top, width: width, height: height })
+    sharp(input)
+      .extract({ left: 0, top: 0, width: 1629, height: 2475 }) //({ left: left, top: top, width: width, height: height })
       // .resize(200,200)
       .toFile("./output/back-page.png")
       .then(() => {
@@ -82,10 +88,26 @@ function sharpBackBPage() {
   );
 }
 
-function sharpFrontPage() {
+function cropPage(input, left, top, width, height, output) {
   return (
-    sharp("./webshot.png")
-      .extract({ left: 1730, top: 0, width: 1595, height: 2475 }) //({ left: left, top: top, width: width, height: height })
+    sharp(input)
+      .extract({ left: left, top: top, width: width, height: height }) //({ left: left, top: top, width: width, height: height })
+      // .resize(200,200)
+      .toFile(`./output/${output}.png`)
+      .then(() => {
+        console.log(`Generating ${output}....`);
+      })
+      .catch(error => {
+        console.log(output, error, "Stack : ", error.stack);
+        throw error;
+      })
+  );
+}
+
+function sharpFrontPage(input) {
+  return (
+    sharp(input)
+      .extract({ left: 1696, top: 0, width: 1629, height: 2475 }) //({ left: left, top: top, width: width, height: height })
       // .resize(200,200)
       .toFile("./output/front-page.png")
       .then(() => {
@@ -98,10 +120,10 @@ function sharpFrontPage() {
   );
 }
 
-function sharpSpinePage() {
+function sharpSpinePage(input) {
   return (
-    sharp("./webshot.png")
-      .extract({ left: 1664, top: 0, width: 66, height: 2475 }) //({ left: left, top: top, width: width, height: height })
+    sharp(input)
+      .extract({ left: 1629, top: 0, width: 67, height: 2475 }) //({ left: left, top: top, width: width, height: height })
       // .resize(200,200)
       .toFile("./output/spine.png")
       .then(() => {
@@ -118,10 +140,10 @@ async function generatePDF(config) {
   try {
     if (config.type === "cover") {
       // HTML_PNG-PDF
-      await webShot(); // takes screenshot
-      await sharpFrontPage(); // crop frontPage
-      await sharpBackBPage(); // crop BackPage
-      await sharpSpinePage(); // crop spinePage
+      await webShot("./template2.html"); // takes screenshot
+      await sharpFrontPage("./webshot.png"); // crop frontPage
+      await sharpBackBPage("./webshot.png"); // crop BackPage
+      await sharpSpinePage("./webshot.png"); // crop spinePage
       await princePdf(); // generate PDF
     } else {
       // HTML-PDF
